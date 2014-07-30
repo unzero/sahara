67cc0fd50c4a02389c16bd3794b0fb04136af62b
Ciro Garcia <ciroivan94@gmail.com>
2014-07-29 22:32:08 -0500

    primera version del proyecto

---
+++ src/core/app.java
@@ -0,0 +1,125 @@
+package core;
+
+import java.util.Scanner;
+import java.util.Random;
+import java.io.*;
+import java.util.LinkedList;
+//DEFINED CLASS
+import database.Conexion;
+import gui.Ventana_Central;
+import gui.select_lecture;
+
+public class app{
+
+private final static int options = 4;
+
+	Scanner reader;
+	Random random_generator;
+	Conexion conexion;
+
+	public app() throws IOException {
+		reader = new Scanner(System.in);
+		random_generator = new Random();
+		conexion = new Conexion();
+	}
+
+	private int read_int(){
+		while(true){
+			String ln = reader.nextLine();
+			try{
+				return Integer.parseInt(ln);
+			}catch(NumberFormatException ex){}
+		}
+	}
+
+	private String read_line(){
+		return reader.nextLine();
+	}
+
+	private void insert(){
+		System.out.println("Por favor ingrese el kanji");
+		System.out.println("Formato: Kanji lectura1 lectura2 ... lecturaN");
+		String line = read_line();
+		//create_kanji(line);
+	}
+
+	private void run(){
+            Ventana_Central vc = new Ventana_Central();
+            vc.setVisible(true);
+		int ok = 0;
+		String wrong = "";
+		for(int x=0;x<10;++x){
+			System.out.println("|-------------------------------------------|");
+			Kanji in_study = conexion.get( random_generator.nextInt(conexion.size()) );
+			int loc = random_generator.nextInt(options);
+			System.out.println("Para el Kanji: "+in_study.get_kanji()+" la lectura correcta es:");
+                        LinkedList<String> op = new LinkedList<String>();
+			for(int i=0;i<options;++i){
+				String nx;
+				if(loc == i){
+					nx = in_study.get_lecture( random_generator.nextInt(in_study.size()) );
+				}else{
+					do{
+						nx = conexion.get_lecture( random_generator.nextInt(conexion.total_lectures()) );
+					}while( in_study.indexOf(nx) != -1);
+				}
+                                op.add(nx);
+				System.out.println(i+". "+nx);
+			}
+                        //sending to the GUI
+                       // vc.setPanel(new select_lecture(in_study.get_kanji(),op));
+                        
+			System.out.print("Por favor ingrese la lectura correcta: ");
+			int read = read_int();
+			if( read == loc ) ok++;
+			else wrong += in_study.get_kanji()+" ";
+			//7437400
+			System.out.print("Las lecturas son: "+in_study.toString());
+			System.out.println("|-------------------------------------------|");
+		}
+		System.out.println(ok+"/10");
+		if( !wrong.equals("")){
+			System.out.println("Deberias repasar: ");
+			System.out.println(wrong);
+		}
+	}
+
+	private void menu(){
+		System.out.println(" 0: Para salir");
+		System.out.println(" 1: Para insertar en el mapa");
+		System.out.println(" 2: Para evaluacion rapida");
+	}
+
+	private void decided(int op){
+		if(op == 1){
+//			insert();
+		}else if(op == 2){
+			run();
+		}else if(op != 0){
+
+		}
+	}
+
+	public void start() throws IOException {
+		System.out.println("Bienvenido por favor ingrese su opción");
+		int i = 0;
+		do{
+			menu();
+			i = read_int();
+			decided(i);
+		}while(i != 0);
+
+	}
+
+	public static void main(String[] args){
+		try{
+			app app_running = new app();
+			app_running.start();
+		}catch(IOException ex){
+			System.out.println("Error al iniciar el programa!");
+			ex.printStackTrace();
+		}
+		
+	}
+}
+
---
+++ src/database/Conexion.java
@@ -0,0 +1,71 @@
+package database;
+
+import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+//DEFINED CLASS
+import core.Kanji;
+
+public class Conexion{
+	
+	/*CLASS FIELDS*/
+	private	LinkedList<Kanji> kanji_list;
+	private LinkedList<String> all_lectures;
+	private String file_path = "/home/unzero/kanji/database/data.dat";
+
+	/*PUBLIC METHODS*/
+	public Conexion() throws IOException{
+		kanji_list = new LinkedList<Kanji>();
+		all_lectures = new LinkedList<String>();
+		read();
+	}
+	
+	public void close() throws IOException{
+		write();
+	}
+
+	public Kanji get(int i){
+		return kanji_list.get(i);
+	}
+
+	public String get_lecture(int i){
+		return all_lectures.get(i);
+	}
+
+	public int size(){
+		return kanji_list.size();
+	}
+
+	public int total_lectures(){
+		return all_lectures.size();
+	}
+
+	/*PRIVATE METHODS*/
+	private void read() throws IOException {
+		BufferedReader buf = new BufferedReader(new InputStreamReader(new FileInputStream(file_path),"UTF-8"));
+		String line;
+		while( (line = buf.readLine() ) != null){
+			if(!line.equals("")){
+				Kanji new_kanji = new Kanji(line);
+				kanji_list.add( new_kanji );
+				for(String el : new_kanji.get_lectures()){
+					if(all_lectures.indexOf(el) == -1)all_lectures.add(el);
+				}
+			}
+		}
+		buf.close();
+	}
+
+	private void write() throws IOException {
+		BufferedWriter buf = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file_path),"UTF-8"));
+		for(int x=0;x<kanji_list.size();++x){
+			buf.write(kanji_list.get(x).toString());
+		}
+		buf.close();
+	}
+}
